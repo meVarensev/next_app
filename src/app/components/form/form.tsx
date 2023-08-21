7ae773df8d6f0ae62f9be64a6704395816c9db9c
meVarensev <vitalik.unit@gmail.com>
2023-08-21 19:09:21 +0500

    feat: get_login

--- src/app/components/form/form.tsx
+++ src/app/components/form/form.tsx
@@ -1,78 +1,90 @@
 'use client';
 
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { ActionIcon, Autocomplete, Box, Group, Text, useMantineTheme } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { IconArrowRight, IconBrandGithubFilled, IconSearch } from '@tabler/icons-react';
 import { FC } from 'react';
+import { GET_LOGIN } from '../../server/get_login';
 
 interface FormProps {
 
 }
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    viewer { 
-    login
-  }
-  }
-`;
+
+interface ViewerData {
+    user: {
+        login: string;
+        name: string;
+    };
+}
+
 
 const Form: FC<FormProps> = () => {
-    const { loading, error, data } = useQuery(GET_LOCATIONS);
+        const form = useForm({
+            initialValues: {
+                login: 'meVarensev',
+            },
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
+        });
+        const { loading, error, data } = useQuery<ViewerData>(GET_LOGIN, {
+            variables: { login: form.values.login },
 
-    console.log(data)
+        });
+        
+        const theme = useMantineTheme();
 
-        // const form = useForm({
-        //     initialValues: {
-        //         login: '',
-        //     },
-        //
-        // });
-        //
-        // const theme = useMantineTheme();
-        //
-        // return (
-        //     <Box
-        //         sx={() => ({
-        //             padding: theme.spacing.xl,
-        //             borderBottom: `1px solid white`,
-        //             display: 'flex',
-        //             justifyContent: 'center',
-        //             alignItems: 'center',
-        //             textAlign: 'center',
-        //             width: '100%',
-        //         })}
-        //         mt='lg'
-        //     >
-        //         <form onSubmit={form.onSubmit((values) => console.log(values))} style={{ width: '75%' }}>
-        //
-        //             <Group mb='md'>
-        //                 <IconBrandGithubFilled size='1.5rem' stroke={1.5} />
-        //                 <Text>GitHub repositories</Text>
-        //             </Group>
-        //
-        //             <Autocomplete
-        //                 data={['React', 'Angular', 'Svelte', 'Vue', 'Riot', 'Next.js', 'Blitz.js']}
-        //                 nothingFound='Nothing found'
-        //                 hoverOnSearchChange
-        //                 icon={<IconSearch size='1.1rem' stroke={1.5} />}
-        //                 radius='xl'
-        //                 size='md'
-        //
-        //
-        //                 rightSection={
-        //                     <ActionIcon size={32} radius='xl' color={theme.primaryColor} variant='light' type='submit'>
-        //                         <IconArrowRight size='1.1rem' stroke={1.5} />
-        //                     </ActionIcon>
-        //                 }
-        //                 placeholder='Search'
-        //
-        //                 {...form.getInputProps('login')}
-        //             />
-        //
-        //         </form>
-        //     </Box>
-        // );
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error : {error.message}</p>;
+
+
+        if (!data?.user) return null;
+        const { login, name } = data?.user;
+
+        return (
+            <Box
+                sx={() => ({
+                    padding: theme.spacing.xl,
+                    borderBottom: `1px solid white`,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                    width: '100%',
+                })}
+                mt='lg'
+            >
+                <Text>{login}</Text>
+                <Text>{name}</Text>
+
+                <form onSubmit={form.onSubmit((value) => form.setValues(value))} style={{ width: '75%' }}>
+
+                    <Group mb='md'>
+                        <IconBrandGithubFilled size='1.5rem' stroke={1.5} />
+                        <Text>GitHub repositories</Text>
+                    </Group>
+
+                    <Autocomplete
+                        data={['React', 'Angular', 'Svelte', 'Vue', 'Riot', 'Next.js', 'Blitz.js']}
+                        nothingFound='Nothing found'
+                        hoverOnSearchChange
+                        icon={<IconSearch size='1.1rem' stroke={1.5} />}
+                        radius='xl'
+                        size='md'
+
+
+                        rightSection={
+                            <ActionIcon size={32} radius='xl' color={theme.primaryColor} variant='light' type='submit'>
+                                <IconArrowRight size='1.1rem' stroke={1.5} />
+                            </ActionIcon>
+                        }
+                        placeholder='Search'
+
+                        {...form.getInputProps('login')}
+                    />
+
+                </form>
+            </Box>
+        );
     }
 ;
 
